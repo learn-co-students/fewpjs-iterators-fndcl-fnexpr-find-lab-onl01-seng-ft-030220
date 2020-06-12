9901192378b02b56375b4f0ec1e10bd32db4d386
gavinsharkey <gavinsharkey2016@gmail.com>
2020-06-12 11:12:38 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const superbowlWin = (games) => {
+  const win = games.find(game => game.result === 'W')
+  if (win) {
+    return win.year
+  }
+}
