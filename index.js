d498fd2f7ab0f8b604656f43fcf1dddba8421645
Dusch4593 <brandondusch@gmail.com>
2020-06-20 13:11:06 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arr) {
+  let winningRecord = arr.find(record => record["result"] == "W");
+  return winningRecord ? winningRecord.year : undefined
+}
